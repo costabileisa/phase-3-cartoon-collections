c16cc510e32e0302d93a78f9b3a92a0eaf2c4694
Isabella Costabile <costabileisa@gmail.com>
2022-12-26 13:29:25 -0500

    commit tree 938d82b340565e176faeb2725c9fa08db1707d96

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves dwarves_array# code an argument here
   # Your code here
+  dwarves_array.to_enum.with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet calls_array# code an argument here
   # Your code here
+  calls_array.map do |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls_array# code an argument here
   # Your code here
+  calls_array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese strings_array# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings_array.find do |string|
+    cheese_types.include?(string)
+  end
 end
